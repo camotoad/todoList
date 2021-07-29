048c0bad870d6acc30e1d7748773cd25bb99fba2
GXY <camotoad1@gmail.com>
2021-07-29 19:16:54 -0400

    working db connection

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
+const db = require('mongoose').model('todo');
+
 //selectors
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
 const filterOption = document.querySelector('.filter-todo');
+const errorText = document.querySelector('.error-text');
 
 //event listeners
+document.addEventListener("DOMContentLoaded", loadTodos);
 todoButton.addEventListener('click', addTodo);
 todoList.addEventListener('click', buttonCheck);
 filterOption.addEventListener('click', filterTodo);
 
+
 //functions
+function resetText(){
+    todoInput.value = '';
+    errorText.innerHTML = '';
+    errorText.style.display = 'none';
+}
+
+function errtxt(text){
+    errorText.style.display = 'block';
+    errorText.innerHTML = '<i class="fa fa-times-circle"></i>' + text;
+}
+
 function addTodo(event){
     event.preventDefault();
 
@@ -19,21 +35,32 @@ function addTodo(event){
     const newTodo = document.createElement('li');
     newTodo.innerText = todoInput.value;
     newTodo.classList.add('todo-item');
-    todoDiv.appendChild(newTodo);
-
+    
     const completedButton = document.createElement('button');
     completedButton.innerHTML = '<i class="fas fa-check"> </i>';
     completedButton.classList.add('completed-btn');
-    todoDiv.appendChild(completedButton);
-
+    
     const deleteButton = document.createElement('button');
     deleteButton.innerHTML = '<i class="fas fa-trash"> </i>';
     deleteButton.classList.add('delete-btn');
-    todoDiv.appendChild(deleteButton);
+    
+    const newEntry = new db({
+        item: todoInput.value,
+        status: false
+    });
 
-    todoList.appendChild(todoDiv);
+    newEntry.save((err) => {
+        if(err){
+            errtxt(err);
+        } else {
+            todoDiv.appendChild(newTodo);
+            todoDiv.appendChild(completedButton);
+            todoDiv.appendChild(deleteButton);
+            todoList.appendChild(todoDiv);
+        }
+    });
 
-    todoInput.value = '';
+    resetText();
 }
 
 function buttonCheck(event){
@@ -42,10 +69,26 @@ function buttonCheck(event){
 
     switch(item.classList[0]){
         case 'delete-btn':
-            todo.remove();
+            //console.log(todo.childNodes[0].innerText);
+            db.findOneAndDelete({item : todo.childNodes[0].innerText }, function(err, item){
+                if(err){ 
+                    errtxt(err);
+                } else {
+                todo.remove();
+                console.log('deleted ' + item);
+                }
+            });       
             break;
         case 'completed-btn':
-            todo.classList.toggle('completed');
+            db.findOneAndUpdate({ item : todo.childNodes[0].innerText }, { status : true} , {upsert: true}, 
+                function(err, item){
+                    if(err){
+                        errtxt(err);
+                    } else {
+                        console.log('completed ' + item)
+                        todo.classList.toggle('completed');
+                    }
+            });           
             break;
     } 
 }
@@ -75,4 +118,40 @@ function filterTodo(event){
                 break;
         }
     });
+}
+
+function loadTodos(){
+    resetText();
+
+    db.find({}, function(err, items) {
+        if(err){
+            errtxt(err);
+        } else {
+            items.forEach((item) => {
+                const todoDiv = document.createElement('div');
+                todoDiv.classList.add('todo');
+
+                const newTodo = document.createElement('li');
+                newTodo.innerText = item.item;
+                newTodo.classList.add('todo-item');
+                todoDiv.appendChild(newTodo);
+
+                const completedButton = document.createElement('button');
+                completedButton.innerHTML = '<i class="fas fa-check"> </i>';
+                completedButton.classList.add('completed-btn');
+                todoDiv.appendChild(completedButton);
+
+                const deleteButton = document.createElement('button');
+                deleteButton.innerHTML = '<i class="fas fa-trash"> </i>';
+                deleteButton.classList.add('delete-btn');
+                todoDiv.appendChild(deleteButton);
+
+                todoList.appendChild(todoDiv);
+
+                if(item.status){
+                    todoDiv.classList.toggle('completed');
+                }
+            });
+        }
+    })
 }
--- server.js
+++ server.js
@@ -1,10 +1,39 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
+//configure express
 app.use(express.static('./'));
 app.get('/', function(req, res){
     res.render('index', { title: 'My Todo list'});
 });
 
+function configureMongoose(){
+    const db = mongoose.connect('mongodb://localhost/todo-db');
+
+    mongoose.connection.on("open", function(ref) {
+        console.log("Connected to mongo server.");
+      });
+      
+    mongoose.connection.on("error", function(err) {
+        console.log("Could not connect to mongo server!");
+        console.log(err);
+      });
+
+    require('./model');
+
+    return db;
+}
+
+//configure mongoose
+const db = configureMongoose();
+
+
 app.listen(3000);
+
 console.log('Server running at http://localhost:3000/');
+
+//testing
+// const testdb = require('mongoose').model('todo');
+// const t = new testdb({item: 'hi', status: true});
+// t.save();
